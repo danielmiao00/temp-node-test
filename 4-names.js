cab48b2bb6e1a2192d1cfeae9d0ce0cd644becef
Daniel Miao - d0m0orw <Daniel.Miao@walmart.com>
2024-08-13 10:18:29 -0500

    first commit

---
+++ 4-names.js
@@ -0,0 +1,10 @@
+//local
+const secret = 'SUPER SECRET' 
+
+// shared variable
+const john = 'test'
+const peter = 'peter'
+
+module.exports = {john, peter}
+
+console.log(module)
---
+++ app.js
@@ -0,0 +1,1 @@
+require('./12-http')
